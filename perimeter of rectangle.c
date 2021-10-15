a689f71334c1a82985843612c4cc7cc273bc810f
Cosmas-pro <91739410+Cosmas-pro@users.noreply.github.com>
2021-10-15 13:02:00 +0300

    Add files via upload

---
+++ perimeter of rectangle.c
@@ -0,0 +1,13 @@
+/*calculate perimeter of rectangle*/
+include<stdio.h>
+int main ()
+{
+int length, width, Perimeter;
+printf("\n Enter value to Length :");
+scanf("%d",&Length);
+printf("\n Enter value to Width :");
+scanf("%d",&Width);
+perimeter =2(Length+width);
+printf("\n Perimeter =&",Perimeter);
+return 0;
+}
